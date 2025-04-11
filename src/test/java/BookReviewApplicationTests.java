2c9f93304ff9482b9fd9303b8cbf863813fe5a42
Monique <monique.borje2@mail.dcu.ie>
2025-04-11 11:00:03 +0100

    Add test

---
+++ src/test/java/BookReviewApplicationTests.java
@@ -0,0 +1,16 @@
+
+
+import com.example.bookreview.BookReviewApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = BookReviewApplication.class)
+public class BookReviewApplicationTests {
+
+    @Test
+    public void contextLoads(){
+
+    }
+}
+
+
